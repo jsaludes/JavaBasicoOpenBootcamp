5b6494ebe255af8da0afe79fda9e1ef585304b90
Jordi Saludes <93613431+saludesj@users.noreply.github.com>
2022-02-08 20:44:23 +0100

    Add files via upload

---
+++ Ejercicios7_8_9-FileInFileOut/src/FileInFileOut.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class FileInFileOut {
+    public static void main(String[] args) {
+
+        finout("/etc/passwd", "prueba.txt");
+    }
+
+    static void finout(String rutaEntrada, String rutaSalida){
+        try{
+            InputStream entrada = new FileInputStream(rutaEntrada);
+            byte [] datos = entrada.readAllBytes();
+
+            PrintStream salida = new PrintStream(rutaSalida);
+            salida.write(datos);
+
+        }catch(IOException e){
+            System.out.println("Error: Revisa que las rutas del archivo de entrada y del archivo de salida sean correctas.");
+        }
+    }
+}
